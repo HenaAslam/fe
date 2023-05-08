dd8e079c780a5c7489321624e2f9779529975c55
ajeer333 <ajeer333@gmail.com>
2023-05-09 00:06:07 +0200

    commit

--- sprintify/src/components/Column.jsx
+++ sprintify/src/components/Column.jsx
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import Task from "./Task";
 
 const Column = ({ column, tasks }) => {
+  const [stateTasks, setStateTasks] = useState(tasks);
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const newStateTasks = Array.from(stateTasks);
+    const [removed] = newStateTasks.splice(source.index, 1);
+    newStateTasks.splice(destination.index, 0, removed);
+
+    setStateTasks(newStateTasks);
+
+    fetch(`${process.env.REACT_APP_BE_URL}/boards/${draggableId}/position`, {
+      method: "PUT",
+      body: JSON.stringify({ position: destination.index }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
     <div>
-      {tasks.map((task, index) => (
-        <Task key={task._id} task={task} index={index} />
-      ))}
+      <h2>{column.title}</h2>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId={column._id}>
+          {(provided, snapshot) => (
+            <div
+              className="task-column"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {stateTasks.map((task, index) => (
+                <Task key={task.id} task={task} index={index} />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
--- sprintify/src/components/Task.jsx
+++ sprintify/src/components/Task.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Draggable } from "react-beautiful-dnd";
 
 const Task = ({ task, index }) => {
-  return <div className="task-inside-column">{task.title}</div>;
+  return (
+    <Draggable draggableId={task._id} index={index}>
+      {(provided, snapshot) => (
+        <div
+          className="task-inside-column"
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {task.title}
+        </div>
+      )}
+    </Draggable>
+  );
 };
 
 export default Task;
